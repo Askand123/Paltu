c4b958a54a79baa4b37be69f56c0e0f03022edd5
Askand123 <askandsingh2oct2008@cmselearning.org>
2021-10-29 19:20:54 +0530

    small change of colour in navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -10,7 +10,7 @@ export default function Navbar() {
             </button>
             <div className="offcanvas offcanvas-start bg-dark" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
             <div className="offcanvas-header">
-                <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Menu</h5>
+                <h5 className="offcanvas-title text-white" id="offcanvasNavbarLabel">Menu</h5>
                 <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
             </div>
             <div className="offcanvas-body">
