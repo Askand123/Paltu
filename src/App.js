2b295c13f2609e611da2d3f87091d54e9e79a829
Vidya Sagar Singh <sagard55555@gmail.com>
2021-10-29 17:27:07 +0530

    changed link name

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React With Master Sagar
+          Learn React With Master Askand
         </a>
       </header>
     </div>
