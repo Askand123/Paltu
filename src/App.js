00330d022167c0c21cb3401e94bd4d25fb81c4b5
Askand123 <askandsingh2oct2008@cmselearning.org>
2021-10-30 21:54:10 +0530

    Add moving image and login form but login form has bug

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 
 import "./App.css";
 import Navbar from "./Components/Navbar";
+import SlidingPoster from "./Components/SlidingPoster"
+
 
 function App() {
   return (
     <>
       <Navbar />
+      <SlidingPoster />
     </>
   );
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,10 +1,27 @@
 import React from 'react'
+import '../SlidingPoster.css'
+import '../Login.css'
 
+let FormBtn = document.querySelector('#user-btn');
+let loginForm = document.querySelector('.login-form-container');
+let FormClose = document.querySelector('#form-close');
+
+window.onscroll = () => {
+    loginForm.classList.remove('active');
+}
+FormBtn.addEventListener('click', () =>{
+    loginForm.classList.add('active');
+});
+FormClose.addEventListener('click', () =>{
+    loginForm.classList.remove('active');
+});
 export default function Navbar() {
     return (
+        <>
         <nav className="navbar navbar-dark bg-dark fixed-top">
         <div className="container-fluid">
             <a className="navbar-brand" href=".">Paltu - Online Shop for Pet Lovers</a>
+            <i className="fas fa-user" id="user-btn"></i>
             <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
             <span className="navbar-toggler-icon"></span>
             </button>
@@ -21,14 +38,14 @@ export default function Navbar() {
                 <li className="nav-item">
                     <a className="nav-link" href=".">Option 2</a>
                 </li>
-                <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbardarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="." id="navbardarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                     Dropdown
                 </a>
-                <ul class="dropdown-menu dropdown-menu-dark mb-5" aria-labelledby="navbardarkDropdownMenuLink">
-                    <li><a class="dropdown-item" href="#">Option 3</a></li>
-                    <li><a class="dropdown-item" href="#">option 4</a></li>
-                    <li><a class="dropdown-item" href="#">option 5</a></li>
+                <ul className="dropdown-menu dropdown-menu-dark mb-5" aria-labelledby="navbardarkDropdownMenuLink">
+                    <li><a className="dropdown-item" href=".">Option 3</a></li>
+                    <li><a className="dropdown-item" href=".">option 4</a></li>
+                    <li><a className="dropdown-item" href=".">option 5</a></li>
                 </ul>
                 </li>
                 </ul>
@@ -39,6 +56,19 @@ export default function Navbar() {
             </div>
             </div>
         </div>
-</nav>
+    </nav>
+                <div className="login-form-container">
+                
+                <i className="fas fa-times" id= 'form-close'></i>
+                <form>
+                    <h3>Sign Up</h3>
+                    <input type="text" className="box" placeholder="Enter Your Full name" required />
+                    <input type="Number" className="box" placeholder="Enter Your Mobile No." required />
+                    <input type="email"className="box" placeholder="Enter Your email" required />
+                    <input type="date" class= 'box' placeholder="enter your date of birth" required />
+                    <input type="submit" value='Submit Now' className='btn' />
+                </form>
+        </div>
+    </>
     );
 }
