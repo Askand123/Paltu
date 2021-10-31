5786bf4b62f866ec1f7571a28cf4f7eb66b13b73
Askand123 <askandsingh2oct2008@cmselearning.org>
2021-10-31 21:28:09 +0530

    reduced the line of css in navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -5,12 +5,12 @@ export default function Navbar() {
     return (
         <div>
             <nav>
-                <div className="Left">
+                <div className="navdiv">
                     <h4>Paltu</h4>
                     <input type="search" placeholder="Search for product, brand and more"/>
                     <i class="fas fa-search searchicon"></i>
                 </div>
-                <div className="Right">
+                <div className="navdiv">
                     <h6 className="Navbtn">Login</h6>
                     <h6>More <i class="fas fa-chevron-down"></i></h6>
                     <h6>Cart <i class="fas fa-shopping-cart"></i></h6>
