4416ff276fefc29903e3d9820f3aa7d9181edcdd
Askand123 <askandsingh2oct2008@cmselearning.org>
2021-10-31 21:25:26 +0530

    completed Navbar design

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import "./navbar.css";
 
-export default function Navbar(props) {
+export default function Navbar() {
     return (
         <div>
             <nav>
                 <div className="Left">
                     <h4>Paltu</h4>
                     <input type="search" placeholder="Search for product, brand and more"/>
-                    <i class="fas fa-search"></i>
+                    <i class="fas fa-search searchicon"></i>
                 </div>
                 <div className="Right">
-                    <h6>{props.firstcontent}</h6>
+                    <h6 className="Navbtn">Login</h6>
                     <h6>More <i class="fas fa-chevron-down"></i></h6>
                     <h6>Cart <i class="fas fa-shopping-cart"></i></h6>
                 </div>
