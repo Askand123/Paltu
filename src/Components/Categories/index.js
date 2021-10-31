ce01a6f86cf893e47673232736a10508ce099c13
Askand123 <askandsingh2oct2008@cmselearning.org>
2021-10-31 20:33:03 +0530

    some bugs

--- src/Components/Categories/index.js
+++ src/Components/Categories/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { categories } from "../../Constants/DummyData";
 import styles from "./Categories.module.css";
+import Navbar from "..//navbar/Navbar";
 
 export default function index() {
   return (
     <div className={styles.container}>
+      <Navbar />
       <div className={styles.top}>
         {categories.map((item, i) => (
           <div key={i} className={styles.avatar}>
